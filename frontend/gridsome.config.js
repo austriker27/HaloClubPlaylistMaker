862c2dfa62dbd6770dc23ca5f1a0d11ccb42e40c
austriker27 <davidalindahl@gmail.com>
2020-01-24 07:17:34 -0800

    adding tailwind config

--- frontend/gridsome.config.js
+++ frontend/gridsome.config.js
@@ -5,20 +5,20 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Woodinville Halofest',
   plugins: [
     {
         use: 'gridsome-plugin-purgecss',
         // default options, the following will be included if you don't provide anything
         options: {
-        content: [
-            './src/**/*.vue',
-            './src/**/*.js',
-            './src/**/*.jsx',
-            './src/**/*.pug',
-            './src/**/*.md'
-        ],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+            content: [
+                './src/**/*.vue',
+                './src/**/*.js',
+                './src/**/*.jsx',
+                './src/**/*.pug',
+                './src/**/*.md'
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
         }
     },
     {
