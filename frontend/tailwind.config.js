f5f461f561701df210b0cbdbb4b3bd885e9fa7dd
austriker27 <davidalindahl@gmail.com>
2020-01-27 13:45:48 -0800

    added color palette ideas

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -34,3 +34,20 @@ module.exports = {
   variants: {},
   plugins: [],
 }
+
+// brand colors: 
+// /* Coolors Exported Palette - coolors.co/677055-aaa776-9f6a25-efe9e6-2a2d22 */
+
+/* HSL */
+// $color1: hsla(80%, 14%, 39%, 1);
+// $color2: hsla(57%, 23%, 56%, 1);
+// $color3: hsla(34%, 62%, 38%, 1);
+// $color4: hsla(20%, 22%, 92%, 1);
+// $color5: hsla(76%, 14%, 15%, 1);
+
+// /* RGB */
+// $color1: rgba(103, 112, 85, 1);
+// $color2: rgba(170, 167, 118, 1);
+// $color3: rgba(159, 106, 37, 1);
+// $color4: rgba(239, 233, 230, 1);
+// $color5: rgba(42, 45, 34, 1);
