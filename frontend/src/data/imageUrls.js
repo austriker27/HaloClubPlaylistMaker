f53ae891aaf72572d9471aac636e70bfa0bc7736
austriker27 <davidalindahl@gmail.com>
2020-01-30 08:19:19 -0800

    image urls updated

--- frontend/src/data/imageUrls.js
+++ frontend/src/data/imageUrls.js
@@ -4,12 +4,12 @@ export default {
         "Hang Em High": {
             name: 'Hang Em High',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/0/03/Hangemhigh.PNG/revision/latest?cb=20080514072638',
         },
         "Chill Out": {
             name: 'Chill Out',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/4/4e/Chill_OutPC.JPG/revision/latest?cb=20070705203835',
         },
         'Blood Gulch': {
             name: 'Blood Gulch',
@@ -19,12 +19,12 @@ export default {
         'Battle Creek': {
             name: 'Battle Creek',
             imageUrl:
-                '',
+                'https://vignette.wikia.nocookie.net/halo/images/6/61/Battle_CreekPC.JPG/revision/latest?cb=20070705202826',
         },
         'Prisoner': {
             name: 'Prisoner',
             imageUrl:
-                '',
+                'https://vignette.wikia.nocookie.net/halo/images/3/37/Prisoner.jpg/revision/latest?cb=20070526071557',
         },
         // HALO 2 MAPS
 		"Lockout": {
@@ -55,140 +55,140 @@ export default {
         'Coagulation': {
 			name: 'Coagulation',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/e/e8/Coagulation.jpg/revision/latest?cb=20060726142209',
         },
         'Sanctuary': {
 			name: 'Sanctuary',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/a/a9/Sanctuary_profile.jpg/revision/latest/scale-to-width-down/700?cb=20100727035851',
         },
         'Colossus': {
 			name: 'Colossus',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/b/b6/Colossusbigger.jpg/revision/latest?cb=20060728003052',
         },
         'Headlong': {
 			name: 'Headlong',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/1/11/1099796178.jpg/revision/latest?cb=20061023175708',
         },
         'Elongation': {
 			name: 'Elongation',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/0/0c/Elongation.gif/revision/latest?cb=20060725121327',
         },
         'Burial Mounds': {
 			name: 'Burial Mounds',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/0/0c/BurialMoundsBigger.jpg/revision/latest?cb=20081011204326',
         },
         'Midship': {
 			name: 'Midship',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/d/dc/Midship.jpg/revision/latest?cb=20060725130441',
         },
         // HALO 2 ANNIVERSARY
         'Lockdown': {
 			name: 'Lockdown',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/7/78/H2A_Lockout_PAX1.jpg/revision/latest?cb=20140828135710',
         },
         'Zenith': {
 			name: 'Zenith',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/3/38/H2A_AscensionAnniversary1.png/revision/latest?cb=20140610175018',
         },
         'Stonetown': {
 			name: 'Stonetown',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/6/6a/H2A_ZanzibarAnniversary4.jpg/revision/latest/scale-to-width-down/700?cb=20140725185308',
         },
         'Bloodline': {
 			name: 'Bloodline',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/a/a4/H2A_Bloodline.jpg/revision/latest?cb=20141006181258',
         },
         'Shrine': {
 			name: 'Shrine',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/4/46/H2A_SanctuaryAnniversary1.jpg/revision/latest?cb=20140813002805',
         },
         // HALO 3 MAPS
         'Valhalla': {
 			name: 'Valhalla',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/b/b5/Val_ext3.jpg/revision/latest?cb=20070518231631',
         },
         'Last Resort': {
 			name: 'Last Resort',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/d/d2/Halo_3_Last_Resort3.jpg/revision/latest?cb=20070704125516',
         },
         'The Pit': {
 			name: 'The Pit',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/f/f8/Thepit_1.jpg/revision/latest?cb=20081206042955',
         },
         'Guardian': {
 			name: 'Guardian',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/3/3d/H3_MP_Guardian_Env4.jpg/revision/latest/scale-to-width-down/700?cb=20070821024506',
         },
         'Narrows': {
 			name: 'Narrows',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/8/89/Narrows01.jpg/revision/latest/scale-to-width-down/700?cb=20081206051820',
         },
         'Standoff': {
 			name: 'Standoff',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/2/25/Standoffoverview.jpg/revision/latest/scale-to-width-down/700?cb=20081206042626',
         },
         'Snowbound': {
 			name: 'Snowbound',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/0/02/Sb_landscape2.jpg/revision/latest/scale-to-width-down/700?cb=20070518230954',
         },
         'High Ground': {
 			name: 'High Ground',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/7/75/High_Ground_Sniper-Gate_View.jpg/revision/latest?cb=20080511142944',
         },
         'Ghost Town': {
 			name: 'Ghost Town',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/6/67/Ghosttownoverhead.jpg/revision/latest/scale-to-width-down/700?cb=20080605132829',
         },
         'Construct': {
 			name: 'Construct',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/4/40/Constructfrontview.jpg/revision/latest?cb=20081206051602',
         },
         // HALO REACH MAPS
         'Boardwalk': {
 			name: 'Boardwalk',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/3/34/HaloReach_-_Boardwalk.jpg/revision/latest?cb=20100912164517',
         },
         'Boneyard': {
 			name: 'Boneyard',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/5/57/HaloReach_-_Boneyard.jpg/revision/latest?cb=20100912164523',
         },
         'Countdown': {
 			name: 'Countdown',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/d/dd/HaloReach_-_Countdown.jpg/revision/latest?cb=20100912164528',
         },
         'Powerhouse': {
 			name: 'Powerhouse',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/9/9d/HaloReach_-_Powerhouse.jpg/revision/latest?cb=20100912164549',
         },
         'Tempest': {
 			name: 'Tempest',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/4/46/Tempest.jpg/revision/latest/scale-to-width-down/700?cb=20101014142331',
         },
     },
 	HALO_GAMES: {
@@ -252,7 +252,7 @@ export default {
         'Infection': {
 			name: 'Infection',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/3/3c/Infection_Symbol.svg/revision/latest/scale-to-width-down/180?cb=20090710215751',
         },
         'Team Duels': {
 			name: 'Team Duels',
@@ -267,7 +267,7 @@ export default {
         'Team Rockets': {
 			name: 'Team Rockets',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/6/61/Kill-rocket-kill.png/revision/latest?cb=20121113175513',
         },
         'Team Fiesta': {
 			name: 'Team Fiesta',
@@ -282,17 +282,17 @@ export default {
         'Team Swords': {
 			name: 'Team Swords',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/8/88/Kill-sword-kill.png/revision/latest/top-crop/width/360/height/450?cb=20121121205011',
         },
         'Team Snipers': {
 			name: 'Team Snipers',
 			imageUrl:
-				'',
+				'https://vignette.wikia.nocookie.net/halo/images/c/c7/Kill-sniper-rifle-kill.png/revision/latest?cb=20121121205011',
         },
         'Tower Of Power': {
 			name: 'Tower Of Power',
 			imageUrl:
-				'',
+				'https://i.ytimg.com/vi/MdU5smwqlaE/maxresdefault.jpg',
         },
     },
 };
