c1d54c93f8f76ec0015d7188d647be6947e69fc5
austriker27 <davidalindahl@gmail.com>
2020-02-09 13:40:13 -0800

    removed commented out code from lambda

--- frontend/functions/submit.js
+++ frontend/functions/submit.js
@@ -1,56 +1,7 @@
-// const Airtable = require('airtable');
 
 exports.handler = function(event, context, callback) {
     callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
+        statusCode: 200,
+        body: "Hello, World"
     });
 }
-
-// const saveUser = async ({ name, email, message }) => {
-//   return new Promise((resolve, reject) => {
-//     const { AT_API_KEY: apiKey, AT_BASE, AT_TABLE } = process.env;
-
-//     Airtable.configure({
-//       apiKey
-//     });
-
-//     const base = Airtable.base(AT_BASE);
-//     // base('2020_02_15').create([
-//     //     {
-//     //         "Game Version": "Halo 2",
-//     //         "Game Map": "Lockout",
-//     //         "Gametype": "Team SWAT"
-//     //     },
-//     // ], function(err, records) {
-//     // if (err) {
-//     //     console.error(err);
-//     //     return;
-//     // }
-//     base(AT_TABLE).create({ name, email, message }, err => {
-//       if (err) return reject(err);
-
-//       resolve();
-//     });
-//   });
-// };
-
-
-        // handleAirtableSubmit(){
-        //     console.log(formData);
-        //     base('Table 1').create([
-        //         {
-        //             "Game Version": "Halo 2",
-        //             "Game Map": "Lockout",
-        //             "Gametype": "Team SWAT"
-        //         },
-        //     ], function(err, records) {
-        //     if (err) {
-        //         console.error(err);
-        //         return;
-        //     }
-        //     records.forEach(function (record) {
-        //         console.log(record.getId());
-        //     });
-        //     });
-        // },
